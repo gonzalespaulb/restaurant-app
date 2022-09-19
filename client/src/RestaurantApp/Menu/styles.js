ca4304e5bc48f19be2e04d6690555c497e002294
gonzalespaulb <76889345+gonzalespaulb@users.noreply.github.com>
2022-09-18 17:54:59 -0700

    needs routing

--- client/src/RestaurantApp/Menu/styles.js
+++ client/src/RestaurantApp/Menu/styles.js
@@ -17,18 +17,20 @@ export const CardContainer = styled.div`
 export const Sidebar = styled.div`
   background: #fefae0;
   border-radius: 10px;
+  height: calc(100vh - 130px);
+  position: sticky;
+  top: 110px;
+  border: 2px dashed #000;
 `;
 
 export const Card = styled.div`
   background: #fefae0;
   cursor: pointer;
   border-radius: 10px;
-  transition: box-shadow 0.7s ease;
+  transition: border 0.2s ease;
+  border: 2px dashed #000;
 
   :hover {
-    box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034),
-      0 6.7px 5.3px rgba(0, 0, 0, 0.048), 0 12.5px 10px rgba(0, 0, 0, 0.06),
-      0 22.3px 17.9px rgba(0, 0, 0, 0.072), 0 41.8px 33.4px rgba(0, 0, 0, 0.086),
-      0 100px 80px rgba(0, 0, 0, 0.12);
+    border: 4px dashed #000;
   }
 `;
--- client/src/RestaurantApp/Navigation/styles.js
+++ client/src/RestaurantApp/Navigation/styles.js
@@ -5,18 +5,25 @@ export const MainContainer = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    position: sticky;
+    top: 0;
+    border-bottom: 2px dashed #000;
 `;
 
 export const PageLinks = styled.span`
     font-size: 24px;
     line-height: 24px;
     cursor: pointer;
+    border: 2px dashed #000;
+    padding: 10px;
+    border-radius: 10px;
 
     :not(:last-child) {
         margin-right: 32px;
     }
 
     :hover {
-        text-decoration: underline;
+        background: #000;
+        color: #fefae0;
     }
 `;
